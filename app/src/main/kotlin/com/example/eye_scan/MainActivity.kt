de89392fe2e91748189be0061749087b13c6de3b
Raj kumar Patel <Rajkumar07793@gmail.com>
2022-01-21 01:06:39 +0530

    Initial commit

---
+++ app/src/main/kotlin/com/example/eye_scan/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.eye_scan
+import com.mantra.mis100.BuildConfig.APPLICATION_ID
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
